ea64f76cfa311b1f81a87acc39826db4b6f005d1
noom4488 <42562829+noom4488@users.noreply.github.com>
2018-08-22 14:32:34 +0700

    Add files via upload

--- bot-train.php
+++ bot-train.php
@@ -45,7 +45,7 @@ if (!is_null($events['events'])) //check ค่าในตัวแปร $even
 				$a=1;
 				while($obj = mysqli_fetch_array($query_area))
 				{
-					$txtsend = $txtsend ."\n\n".$a.".".$obj["oper"]."\n".$obj['area']."\n".$obj["wbs"]."\n".$obj["name"];
+					$txtsend = $txtsend ."\n\n".$a.".".$obj["oper"]."\n".$obj['area']."\n".$obj["wbs"]."\n".$obj["name"].echo"พอแล้ว";
 					$a = $a+1;
 				}
 				reply_msg($txtsend,$replyToken);//เรียกใช้ function
