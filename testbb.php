b4ea82bbe7a4ae0bda4f65ee9d14e829f4e9c4e1
noom4488 <42562829+noom4488@users.noreply.github.com>
2018-08-22 10:17:08 +0700

    Add files via upload

--- testbb.php
+++ testbb.php
@@ -1,8 +1,8 @@
 <?php
  require('./db/conect-db.php');//เรียกใช้ไฟร์
- $sql_office="SELECT *form tbl_office";
+ $sql_office="SELECT * from tbl_office";
  $query_office=mysqli_query($conn,$sql_office);
- while($obj)=mysqli_fetch_array($query_office))
+ while($obj=mysqli_fetch_array($query_office))
  {
 	 echo $obj["office_name"]."<br>";
  }
